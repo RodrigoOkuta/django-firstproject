9113099851c5b89e1d18571a7e926e2eae95e208
Rodrigo Okuta <rodrigo.okuta@gmail.com>
2021-09-09 22:49:28 -0400

    template loads with app

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+    context = {
+        'object': obj
+    }
+    return render(request, "products/products_detail.html", context)
