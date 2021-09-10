a5d6f640bf643e26e8790beefc798b76581e4440
Rodrigo Okuta <rodrigo.okuta@gmail.com>
2021-09-10 17:56:14 -0400

    forms

--- src/products/forms.py
+++ src/products/forms.py
@@ -4,6 +4,49 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(
+        attrs={
+            "placeholder": "Your Title"
+        }
+    ))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "new-class-name two",
+                "id": 'my_id_for_textarea',
+                "placeholder": "Your Description",
+                "rows": 20,
+                "columns": 120
+            }))
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = ['title', 'description', 'price']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "CFE" in title:
+            raise forms.ValidationError("This is not a valid title")
+
+        return title
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(
+        attrs={
+            "placeholder": "Your Title"
+        }
+    ))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "new-class-name two",
+                "id": 'my_id_for_textarea',
+                "placeholder": "Your Description",
+                "rows": 20,
+                "columns": 120
+            }))
+    price = forms.DecimalField(initial=199.99)
